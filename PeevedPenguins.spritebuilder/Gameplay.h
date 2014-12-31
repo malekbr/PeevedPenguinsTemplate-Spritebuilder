3f4db66e000b82a481d455eb9580ac3702712263
Malek Ben Romdhane <malek@Mings-MacBook-Air.local>
2014-12-31 18:52:29 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Malek Ben Romdhane on 12/31/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
