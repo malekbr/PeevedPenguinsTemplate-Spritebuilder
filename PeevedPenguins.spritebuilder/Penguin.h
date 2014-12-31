4029859d52ce0170f3b86232a598728b241877fe
Malek Ben Romdhane <malek@Mings-MacBook-Air.local>
2014-12-31 14:15:54 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Penguin.h
@@ -0,0 +1,13 @@
+//
+//  Penguin.h
+//  PeevedPenguins
+//
+//  Created by Malek Ben Romdhane on 12/31/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Penguin : CCSprite
+
+@end
