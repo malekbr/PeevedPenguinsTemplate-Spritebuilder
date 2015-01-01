7ea60a3621de5df32d7eb37154afdc29c603cbe2
Malek Ben Romdhane <malek@Mings-MacBook-Air.local>
2015-01-01 16:17:24 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/WaitingPenguin.m
+++ PeevedPenguins.spritebuilder/WaitingPenguin.m
@@ -9,5 +9,11 @@
 #import "WaitingPenguin.h"
 
 @implementation WaitingPenguin
-
+- (void)didLoadFromCCB
+{
+    // generate a random number between 0.0 and 2.0
+    float delay = (arc4random() % 2000) / 1000.f;
+    // call method to start animation after random delay
+    [self performSelector:@selector(startBlinkAndJump) withObject:nil afterDelay:delay];
+}
 @end
