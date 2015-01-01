f27b2d8a7cccc288d6b29347a11ff3aa2f02859f
Malek Ben Romdhane <malek@Mings-MacBook-Air.local>
2015-01-01 16:26:09 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/WaitingPenguin.m
+++ PeevedPenguins.spritebuilder/WaitingPenguin.m
@@ -16,4 +16,11 @@
     // call method to start animation after random delay
     [self performSelector:@selector(startBlinkAndJump) withObject:nil afterDelay:delay];
 }
+- (void)startBlinkAndJump
+{
+    // the animation manager of each node is stored in the 'animationManager' property
+    CCAnimationManager* animationManager = self.animationManager;
+    // timelines can be referenced and run by name
+    [animationManager runAnimationsForSequenceNamed:@"BlinkAndJump"];
+}
 @end
